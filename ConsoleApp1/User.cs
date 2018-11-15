c0f96224ce130006615ea383ded9cdc9ef540cfc
qizl <q@qizl.cn>
2018-11-15 17:26:11 +0800

    -

---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
